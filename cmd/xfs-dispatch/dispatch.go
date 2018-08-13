cfc2740807ccd99bcb5e5ae961e6efcf84479382
Eeo Jun <141bytes@gmail.com>
2018-08-13 22:58:06 +0800

    use structured logging

--- cmd/xfs-dispatch/dispatch.go
+++ cmd/xfs-dispatch/dispatch.go
@@ -1,11 +1,19 @@
 package main
 
 import "encoding/json"
-import "fmt"
 import "os"
 import "os/exec"
+import "go.uber.org/zap"
 import "github.com/mitchellh/go-homedir"
 import "github.com/eugene-eeo/xfs/libxfs"
+
+func logError(logger *zap.SugaredLogger, rid int, err error) {
+	logger.Errorw(
+		"error",
+		"rid", rid,
+		"err", err,
+	)
+}
 
 func handle(event libxfs.Event, d *libxfs.Dispatcher) error {
 	switch event.Type {
@@ -14,11 +22,11 @@ func handle(event libxfs.Event, d *libxfs.Dispatcher) error {
 		// dispatch event.Src | xfs-index set event.Src checksum
 		checksum, err := libxfs.GetSHA256ChecksumFromFile(event.Src)
 		if err != nil {
-			return nil
+			return err
 		}
 		mimetype, err := libxfs.MimetypeFromFile(event.Src)
 		if err != nil {
-			return nil
+			return err
 		}
 		handler, found := d.Match(mimetype)
 		if !found {
@@ -26,27 +34,27 @@ func handle(event libxfs.Event, d *libxfs.Dispatcher) error {
 		}
 		file, err := os.Open(event.Src)
 		if err != nil {
-			return nil
+			return err
 		}
 		defer file.Close()
 		cmd := exec.Command(handler, event.Src)
 		ind := exec.Command("bin/xfs-index", "set", event.Src, checksum)
 		ind.Stdin, _ = cmd.StdoutPipe()
-		ind.Start()
-		err = cmd.Run()
-		if err != nil {
-			fmt.Println(err)
-		}
-		err = ind.Wait()
-		if err != nil {
-			fmt.Println(err)
-		}
+		_ = ind.Start()
+		_ = cmd.Run()
+		_ = ind.Wait()
 	case libxfs.Delete:
 		// xfs-index del event.Src
-		exec.Command("bin/xfs-index", "del", event.Src).Run()
+		err := exec.Command("bin/xfs-index", "del", event.Src).Run()
+		if err != nil {
+			return err
+		}
 	case libxfs.Rename:
 		// xfs-index move event.Src event.Dst
-		exec.Command("bin/xfs-index", "move", event.Src, event.Dst).Run()
+		err := exec.Command("bin/xfs-index", "move", event.Src, event.Dst).Run()
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
@@ -65,19 +73,33 @@ func main() {
 		panic(err)
 	}
 	d := json.NewDecoder(os.Stdin)
+	i := 0
+	sugar := zap.NewExample().Sugar()
+	defer sugar.Sync()
 	for {
 		e := libxfs.Event{}
 		err = d.Decode(&e)
 		if err == nil {
-			fmt.Printf(
-				"%s src=%s dst=%s\n",
-				libxfs.PrettifyEventType(e.Type),
-				libxfs.PrettifyPath(home, e.Src),
-				libxfs.PrettifyPath(home, e.Dst),
-			)
-			go handle(e, dispatcher)
-		} else {
-			fmt.Println(err)
+			i++
+			rid := i
+			go func() {
+				sugar.Infow(
+					"new request",
+					"rid", rid,
+					"type", libxfs.PrettifyEventType(e.Type),
+					"src", libxfs.PrettifyPath(home, e.Src),
+					"dst", libxfs.PrettifyPath(home, e.Dst),
+				)
+				err := handle(e, dispatcher)
+				if err == nil {
+					return
+				}
+				sugar.Errorw(
+					"error handling request",
+					"rid", rid,
+					"err", err,
+				)
+			}()
 		}
 	}
 }
